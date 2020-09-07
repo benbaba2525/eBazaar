08407022856ccc2a364b9d360d773b72c3bd85cf
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-09-07 10:36:39 -0700

    changed font size

--- client/src/product/Categories.js
+++ client/src/product/Categories.js
@@ -96,7 +96,7 @@ class Categories extends Component {
           <div className={classes.root}>
             <GridList className={classes.gridList} cols={4}>
               {this.props.categories.map((tile, i) => (
-                <GridListTile key={i} className={classes.tileTitle} style={{ height: '50px', backgroundColor: this.state.selected === tile ? '#f06292' : '#b2ebf2' }}>
+                <GridListTile key={i} className={classes.tileTitle} style={{ height: '50px',backgroundColor: this.state.selected === tile ? '#f06292' : '#b2ebf2' }}>
                   <span className={classes.link} onClick={this.listbyCategory(tile)}>{tile}  <Icon className={classes.icon}>{this.state.selected === tile && 'arrow_drop_down'}</Icon></span>
                 </GridListTile>
               ))}
